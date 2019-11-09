22d1aec756ba6b41116d7bfb484e975e1358eb75
krt23 <kristina2301@ukr.net>
2019-11-09 12:35:10 +0200

    Updates

---
+++ precache-manifest.c65dd72b205db35b387bc2b8e141c6ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db4e3227e9c3b887ea41eac017b4f9d",
+    "url": "/FoodBlog/home/index.html"
+  },
+  {
+    "revision": "4bc34165240ff400844e",
+    "url": "/FoodBlog/home/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "256b753d078a3e6e8f05",
+    "url": "/FoodBlog/home/static/css/main.bd399680.chunk.css"
+  },
+  {
+    "revision": "4bc34165240ff400844e",
+    "url": "/FoodBlog/home/static/js/2.076a90c6.chunk.js"
+  },
+  {
+    "revision": "256b753d078a3e6e8f05",
+    "url": "/FoodBlog/home/static/js/main.1077b586.chunk.js"
+  },
+  {
+    "revision": "40085cefe59231121bab",
+    "url": "/FoodBlog/home/static/js/runtime-main.52257d38.js"
+  },
+  {
+    "revision": "90ccf6e270aebad9564d12ea6f098d97",
+    "url": "/FoodBlog/home/static/media/blog-about.90ccf6e2.jpg"
+  },
+  {
+    "revision": "c1f81c44a5a69aa3b32c38ace37c65b5",
+    "url": "/FoodBlog/home/static/media/blog-contact.c1f81c44.jpg"
+  },
+  {
+    "revision": "7b6804f0227a41da467c74c1d4c16d59",
+    "url": "/FoodBlog/home/static/media/blog-dishes.7b6804f0.jpg"
+  },
+  {
+    "revision": "3da39ac5bb7c9d88b074e31a6bef1a4d",
+    "url": "/FoodBlog/home/static/media/blog-home.3da39ac5.jpg"
+  },
+  {
+    "revision": "895fadcf5a2662966157019f7350c8c8",
+    "url": "/FoodBlog/home/static/media/blog-login.895fadcf.jpg"
+  },
+  {
+    "revision": "bdbbd061ce783b6de6566ba09ff73a33",
+    "url": "/FoodBlog/home/static/media/blog-team.bdbbd061.jpg"
+  }
+]);
